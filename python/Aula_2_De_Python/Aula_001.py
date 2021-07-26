315ced301107c6bb8a91a5036d9fd03b98c08b03
Marcos Alan <alanacessibilidadetotal@gmail.com>
2021-07-26 15:10:04 -0300

    atualização das aulas
    Atualização da aula 2 de tipos primitivos.

---
+++ python/Aula_2_De_Python/Aula_001.py
@@ -0,0 +1,8 @@
+#Tipos primitivos flutuantes.
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número"))
+
+s = n1 / n2
+
+print(f" A divisão entre {n1:.2f} e {n2:.2f} é igual hà {s:.2f}")
