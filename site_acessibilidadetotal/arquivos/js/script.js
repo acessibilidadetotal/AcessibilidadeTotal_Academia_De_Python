ecb26f9ad3fe3c12c44f047719c054298247c1aa
Marcos Alan <alanacessibilidadetotal@gmail.com>
2021-08-19 21:17:55 -0300

    novo projeto
    Projeto Node JS faculdade

---
+++ site_acessibilidadetotal/arquivos/js/script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function {
+    sidebar = document.querySelector ".sidebar".classList.toggle "show-menu";
+})
